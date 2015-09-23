84706b27a01126bd749ead7d02c55c8299a62e9c
unknown <mysticstaff@yahoo.com>
2015-09-23 15:31:48 +0900

    Commit # 1 for Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function was created for the Programming Assignment 2 of the
+## Coursera R Programming class, 2015/09/23
 
-## Write a short comment describing this function
-
+## This function is almost exactly the same as the makeVec function.
+## There are 4 functions: setMatrix, getMatrix, setInverse and getInverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  setMatrix <- function (y = matrix()){
+      x <<- y 
+      inv <<- null
+  }
+  getMatrix <- function () x
+  setInverse <- function(inv) i <<- inv
+  getInverse <- function() i
+  
+  list(setMatrix = setMatrix, 
+       getMatrix = getMatrix,
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function is also almost the same as cachemean,
+## except that the function calculates the inverse of the matrix
+## instead of the mean. It is assumed that the matrix is square.
+## Otherwise 'solve' may be unable to calculate the inverse.
+## No particular code is added to check that the matrix is square.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)){
+    message("getting cached matrix")
+    return(i)
+  }
+  data <- x$getMatrix()
+  i <- solve(data)
+  x$setInverse(i)
+  i
 }
